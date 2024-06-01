5f7068d08d31ece1077e6f660cac7c8779745dc7
Khoa <nvkhoa05@gmail.com>
2024-06-01 21:52:53 +0700

    Create assignment-01

---
+++ src/main/java/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        Department department = new Department();
+        department.id = 1;
+        department.name = "Bảo vệ";
+        System.out.println("Tên phòng ban: " + department.name);
+    }
+}
