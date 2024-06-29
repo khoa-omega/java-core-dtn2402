ed45c2e79f6d96d5d5b92848f4f7b8ddb9f963a2
Khoa <nvkhoa05@gmail.com>
2024-06-29 20:24:00 +0700

    Update

--- src/main/java/com/vti/repository/IUserRepository.java
+++ src/main/java/com/vti/repository/IUserRepository.java
@@ -7,9 +7,20 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface IUserRepository {
+    /**
+     * @return Trả về tất cả user trong database
+     * @throws SQLException Nếu xảy ra lỗi về SQL
+     * @throws IOException Nếu xảy ra lỗi cấu hình
+     */
     List<User> findAll()
             throws SQLException, IOException;
 
+    /**
+     * @param id Định danh user
+     * @return Thông tin user theo id
+     * @throws SQLException Nếu xảy ra lỗi về SQL
+     * @throws IOException Nếu xảy ra lỗi cấu hình
+     */
     User findById(int id)
             throws SQLException, IOException;
 
